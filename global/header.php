bad6e64b70b779934f858e2651e511c6b4befa15
UvTonny <pg@urvenue.com>
2015-04-03 20:30:14 -0600

    Scholar Project
    Version : 1.0
    Author  : pg@urvenue.com
    date     : Friday, April 03, 2015

---
+++ global/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Basic Page Needs
+================================================== -->
+    <meta charset="utf-8">
+    <title><?php echo $title;?></title>
+    <meta name="description" content="Gammer Morelia">
+    <meta name="author" content="pg@urvenue.com">
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="../css/general.css">
+    <!-- JS
+    ================================================== -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <!-- Header
+    ================================================== -->
+</head>
+<body>
---
+++ home/index.php
@@ -0,0 +1,11 @@
+<?php 
+// Global Vars
+$title = "Home";
+//==============================
+include("../global/header.php");
+?>
+<section id="main-home">
+</section>
+<?php
+include("../global/header.php");
+?>
